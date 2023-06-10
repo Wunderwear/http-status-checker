9e7acd05dbc1616692edafa8e6a220bc3e1d87d1
UseYourBrain <38103449+useyourbra1n@users.noreply.github.com>
2023-06-10 12:19:20 +0700

    Add files via upload

---
+++ 200.py
@@ -0,0 +1,34 @@
+import requests
+import concurrent.futures
+
+def check_link(link):
+    response = requests.get(link)
+    if response.status_code == 200:
+        print(f"{link} ok")
+        create_folder = open("result_ok.txt", "a") #file result
+        create_folder.write(f'{link}\n')
+    else:
+        print(f"{link} bad")
+
+def check_links(list):
+    with open(list, 'r') as file:
+        links = file.read().splitlines()
+
+    with concurrent.futures.ThreadPoolExecutor(50) as executor: #thread
+        executor.map(check_link, links)
+
+print(("""
+ /$$   /$$                   /$$     /$$                            /$$$$$$$                     /$$          
+| $$  | $$                  |  $$   /$$/                           | $$__  $$                   |__/          
+| $$  | $$  /$$$$$$$  /$$$$$$\  $$ /$$//$$$$$$  /$$   /$$  /$$$$$$ | $$  \ $$  /$$$$$$  /$$$$$$  /$$ /$$$$$$$ 
+| $$  | $$ /$$_____/ /$$__  $$\  $$$$//$$__  $$| $$  | $$ /$$__  $$| $$$$$$$  /$$__  $$|____  $$| $$| $$__  $$
+| $$  | $$|  $$$$$$ | $$$$$$$$ \  $$/| $$  \ $$| $$  | $$| $$  \__/| $$__  $$| $$  \__/ /$$$$$$$| $$| $$  \ $$
+| $$  | $$ \____  $$| $$_____/  | $$ | $$  | $$| $$  | $$| $$      | $$  \ $$| $$      /$$__  $$| $$| $$  | $$
+|  $$$$$$/ /$$$$$$$/|  $$$$$$$  | $$ |  $$$$$$/|  $$$$$$/| $$      | $$$$$$$/| $$     |  $$$$$$$| $$| $$  | $$
+ \______/ |_______/  \_______/  |__/  \______/  \______/ |__/      |_______/ |__/      \_______/|__/|__/  |__/          
+
+ GitHub : https://github.com/useyourbra1n                                                                                                                                                                                                 
+"""))
+
+list = input(f" Input File List : ")
+check_links(list)
