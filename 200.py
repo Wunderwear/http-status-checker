17aea90c38412104c47ef6bb764a53586682d2fa
Wunderwear <38103449+Wunderwear@users.noreply.github.com>
2024-08-01 15:51:14 +0700

    Update 200.py
    Updating timeout response

--- 200.py
+++ 200.py
@@ -1,24 +1,31 @@
 import requests
 import concurrent.futures
+import time
 
 def check_link(link):
-    response = requests.get(link)
-    if response.status_code == 200:
-        print(f"{link} ok")
-        create_folder = open("result_ok.txt", "a") #file result
+    try:
+        response = requests.get(link, timeout=3)  # set timeout to 3 seconds
+        if response.status_code == 200:
+            print(f"{link} ok")
+            create_folder = open("result_ok.txt", "a")
+            create_folder.write(f'{link}\n')
+        else:
+            print(f"{link} bad")
+            create_folder = open("result_bad.txt", "a")
+            create_folder.write(f'{link}\n')
+    except requests.exceptions.Timeout:
+        print(f"{link} timed out")
+        create_folder = open("result_timeout.txt", "a")
         create_folder.write(f'{link}\n')
-    else:
-        print(f"{link} bad")
-        create_folder = open("result_bad.txt", "a") #file result
-        create_folder.write(f'{link}\n')
+
 def check_links(list):
     with open(list, 'r') as file:
         links = file.read().splitlines()
 
-    with concurrent.futures.ThreadPoolExecutor(50) as executor: #thread
+    with concurrent.futures.ThreadPoolExecutor(50) as executor:
         executor.map(check_link, links)
 
-print(("""
+print("""
  /$$      /$$                           /$$                                                                
 | $$  /$ | $$                          | $$                                                                
 | $$ /$$$| $$ /$$   /$$ /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$  /$$  /$$  /$$  /$$$$$$   /$$$$$$   /$$$$$$ 
@@ -27,9 +34,9 @@ print(("""
 | $$$/ \  $$$| $$  | $$| $$  | $$| $$  | $$| $$_____/| $$      | $$ | $$ | $$| $$_____/ /$$__  $$| $$      
 | $$/   \  $$|  $$$$$$/| $$  | $$|  $$$$$$$|  $$$$$$$| $$      |  $$$$$/$$$$/|  $$$$$$$|  $$$$$$$| $$      
 |__/     \__/ \______/ |__/  |__/ \_______/ \_______/|__/       \_____/\___/  \_______/ \_______/|__/      
-                                                                                                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                                                                         
  GitHub : https://github.com/Wunderwear                                                                                                                                                                                                 
-"""))
+""")
 
 list = input(f" Input File List : ")
 check_links(list)
